27cf4702589343bc8ddc306068991b259512dcde
Andy <alienfreek@gmail.com>
2018-10-22 23:00:10 -0700

    Proper reflecting of ball off of square. (#1)

--- gameloop.js
+++ gameloop.js
@@ -1,10 +1,62 @@
 gameUpdate = function()
 {
+    console.log("xvel ",myBall.xVel,"yvel ",myBall.yVel);
     requestAnimationFrame(gameUpdate);
     if (collisionCheck_Ball_Square(myBall,mySquare)){
-        myBall.xVel=myBall.xVel*(-1);
-        myBall.yVel=myBall.yVel*(-1);
+        var dx = myBall.x - mySquare.x-mySquare.w/2
+        var dy = myBall.y - mySquare.y-mySquare.w/2
+        var width= (myBall.radius*2+mySquare.w)/2; // Distance between the X positions of square and circle center
+        var height= (myBall.radius*2+mySquare.h)/2; // Distance between Y positions of same. If dy<height, they are touching
+        var crossWidth=width*dy;
+        var crossHeight=height*dx;
+        var collision='none';
+        //console.log("xvel ",myBall.xVel,"yvel ",myBall.yVel);
+
+        //If they are touching
+        if(Math.abs(dx)<=width && Math.abs(dy)<=height){
+        //IDK whats going on here but it tells which side you bumped into
+        if(crossWidth>crossHeight){
+            collision=(crossWidth>(-crossHeight))?'bottom':'left';
+        }
+        else{
+            collision=(crossWidth>-(crossHeight))?'right':'top';
+        }
+        }
+        // Manually set the sign for velocity. Not doing so can make the ball jitter in place inside the square.
+        // If colliding with the bottom, the ball has to be coming from the bottom, aka Y velocity < 0.
+        // If Y velocity was > 0 and hitting the bottom, that would mean it is coming from inside the square, which we don't want.
+        // This way, if the ball gets inside the square, it has a quick way of getting back out without pingponging back in.
+        //
+        // Keep in mind javascripts weird coordinate system where top and left are negative, bottom and right are positive.
+        // So a ball going up has a negative Y velocity.
+        if (collision==='bottom'){
+            if(myBall.yVel<0){
+                myBall.yVel=(Math.abs(myBall.yVel));
+            }
+            //console.log("bottom");
+        }
+        else if (collision ==='top'){
+            if(myBall.yVel>0){
+                myBall.yVel=-(Math.abs(myBall.yVel));
+            }
+            //console.log("top");
+        }
+        else if(collision==='right'){
+            if(myBall.xVel<0){
+                myBall.xVel=(Math.abs(myBall.xVel));
+            }
+            //console.log("right");
+        }
+        else if(collision==='left'){
+            if(myBall.xVel>0){
+                myBall.xVel=-(Math.abs(myBall.xVel));
+            }
+            //console.log("left");
+        }
+
     }
+
+
 }
 
 
